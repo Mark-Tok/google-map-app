759330b8e3f22b7a9571df8638ce5a2dd62a55d0
Mark-Tok <marktokarew@gmail.com>
2020-02-10 16:21:45 +0300

    edit List to functional component

--- src/components/List.js
+++ src/components/List.js
@@ -1,90 +1,84 @@
-import {DragDropContext,Draggable,Droppable} from 'react-beautiful-dnd';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import React from 'react';
 
 const getItemStyle = (isDragging, draggableStyle) => ({
-    userSelect: "none",
-    background: isDragging ? "lightgreen" : "#0062ff",
-    color: "white",
-    width: "244px",
-    margin: "10px 0px",
-    padding: "5px 0px",
-    ...draggableStyle
-  });
-  
-  const getListStyle = isDraggingOver => ({
-    background: isDraggingOver ? "#f0f0f0" : "#ffffff",
-  });
-  
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-    return result;
-  };
-  
-class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onDragEnd = this.onDragEnd.bind(this);
-        this.deletItem = this.deletItem.bind(this);
+  userSelect: "none",
+  background: isDragging ? "lightgreen" : "#0062ff",
+  color: "white",
+  width: "244px",
+  margin: "10px 0px",
+  padding: "5px 0px",
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? "#f0f0f0" : "#ffffff",
+});
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+function List(props) {
+
+  const onDragEnd = (result) => {
+    if (!result.destination) {
+      return;
     }
+    const items = reorder(
+      props.list,
+      result.source.index,
+      result.destination.index
+    );
+    props.dragItemProps(items)
+  }
 
-    onDragEnd(result) {
-        if (!result.destination) {
-          return;
-        }
-        const items = reorder(
-          this.props.list,
-          result.source.index,
-          result.destination.index
-        );
-        this.props.dragItemProps(items)
-      }
+ const deletItem = (index) => {
+    props.list.splice(index, 1)
+    props.dragItemProps(props.list)
+  }
 
-    deletItem(index) {
-        this.props.list.splice(index,1)
-        this.props.dragItemProps(this.props.list)
-    }  
-      
-    render() {
-      return (
-        <div className="list__items">
-          <DragDropContext onDragEnd={this.onDragEnd}>
-            <Droppable  droppableId="droppable">
-              {(provided, snapshot) => (
-                <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
-                  {this.props.list.map((item, index) => (
-                      <div>
-                    <Draggable key={index} draggableId={`id-${index}`} index={index}>
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={getItemStyle(
-                            snapshot.isDragging,
-                            provided.draggableProps.style
-                          )}>
-                           <div className="list__itemWrapper">
-                              <div className="list__item">
-                                {item.value}
-                              </div>
-                              <div className="list__button">
-                                <button className="list__buttonDelete" onClick={()=>{this.deletItem(index)}}></button>
-                              </div>
-                            </div>
+  return (
+    <div className="list__items">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div ref={provided.innerRef} style={getListStyle(snapshot.isDraggingOver)}>
+              {props.list.map((item, index) => (
+                <div>
+                  <Draggable key={index} draggableId={`id-${index}`} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}>
+                        <div className="list__itemWrapper">
+                          <div className="list__item">
+                            {item.value}
                           </div>
-                          )}
-                    </Draggable>   
-                    </div>
-                  ))}
-                  {provided.placeholder}
+                          <div className="list__button">
+                            <button className="list__buttonDelete" onClick={() => { deletItem(index) }}></button>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                  </Draggable>
                 </div>
-                )}
-            </Droppable>
-          </DragDropContext>
-        </div>  
-      )
-    } 
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  )
 }
+
 export default List;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Map,InfoWindow,Marker,GoogleApiWrapper,Polyline} from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper, Polyline } from 'google-maps-react';
 import List from './List.js'
 
-const API_KEY = 'API_KEY_HERE'
+const API_KEY = 'AIzaSyCEMRXxrGO6v2B-jBiwduxMLQvDsRxt2YY'
 
 class MapContainer extends React.Component {
   constructor(props) {
@@ -17,16 +17,10 @@ class MapContainer extends React.Component {
       activeMarker: {},
       title: ''
     }
-    this.centerMoved = this.centerMoved.bind(this);
-    this.addMarker = this.addMarker.bind(this);
-    this.onMarkerClick = this.onMarkerClick.bind(this);
-    this.dragMarker = this.dragMarker.bind(this);
-    this.dragItem = this.dragItem.bind(this);
-    this.addMarkerKeyEnter = this.addMarkerKeyEnter.bind(this);
     this.inputRef = React.createRef();
   }
 
-  centerMoved(mapProps, map) {
+  centerMoved = (mapProps, map) => {
     this.setState({
       position: {
         lat: map.getCenter().lat(),
@@ -35,69 +29,69 @@ class MapContainer extends React.Component {
     })
   }
 
-  addMarkerKeyEnter(event) {
-    if(event.keyCode === 13) {
+  addMarkerKeyEnter = (event) => {
+    if (event.keyCode === 13) {
       this.addMarker()
     }
   }
 
-  addMarker() {
-    if(this.inputRef.current.value === '') {
+  addMarker = () => {
+    if (this.inputRef.current.value === '') {
       alert('Введите название пункта назначения')
     }
     else {
-    if(this.state.items.length === 0 || this.state.length < 1) {
-     this.setState({
-      position: {
-        lat: this.state.position.lat,
-        lng: this.state.position.lng,
-        value: this.inputRef.current.value
-        },
-      }, () => {
-        this.setState({
-          items: [...this.state.items, this.state.position]
-        })
-    })
-    this.inputRef.current.value = '';
-    }
-    else {
-      let objCoord = this.state.items.map((item)=> {
-        return item
-      });
-      let currentPosition = this.state.position;
-      let inputValue = this.inputRef.current.value;
-      function findClone(element) {
-        return element.lat === currentPosition.lat || element.value === inputValue
-      }
-      let clone = objCoord.find(findClone)
-      if(clone !== undefined) {
-        if (clone.lat === this.state.position.lat) {
-          alert('В данной точке уже есть маркер, выберете другую точку на карте');
-          this.inputRef.current.value = '';
-        }
-        else if (clone.value === this.inputRef.current.value) {
-          alert(`Маркер ${clone.value} уже существует, выберете другое название`)
-          this.inputRef.current.value = '';
-        }
-      }
-      else {
+      if (this.state.items.length === 0 || this.state.length < 1) {
         this.setState({
           position: {
             lat: this.state.position.lat,
             lng: this.state.position.lng,
             value: this.inputRef.current.value
+          },
+        }, () => {
+          this.setState({
+            items: [...this.state.items, this.state.position]
+          })
+        })
+        this.inputRef.current.value = '';
+      }
+      else {
+        let objCoord = this.state.items.map((item) => {
+          return item
+        });
+        let currentPosition = this.state.position;
+        let inputValue = this.inputRef.current.value;
+        function findClone(element) {
+          return element.lat === currentPosition.lat || element.value === inputValue
+        }
+        let clone = objCoord.find(findClone)
+        if (clone !== undefined) {
+          if (clone.lat === this.state.position.lat) {
+            alert('В данной точке уже есть маркер, выберете другую точку на карте');
+            this.inputRef.current.value = '';
+          }
+          else if (clone.value === this.inputRef.current.value) {
+            alert(`Маркер ${clone.value} уже существует, выберете другое название`)
+            this.inputRef.current.value = '';
+          }
+        }
+        else {
+          this.setState({
+            position: {
+              lat: this.state.position.lat,
+              lng: this.state.position.lng,
+              value: this.inputRef.current.value
             },
           }, () => {
             this.setState({
               items: [...this.state.items, this.state.position]
             })
-        })
-        this.inputRef.current.value = '';
+          })
+          this.inputRef.current.value = '';
         }
       }
     }
   }
-    
+
   onMarkerClick = (props, marker, e) => {
     this.setState({
       title: props.name,
@@ -106,7 +100,7 @@ class MapContainer extends React.Component {
     })
   };
 
-  dragMarker(coord, index, item) {
+  dragMarker = (coord, index, item) => {
     let marker = {
       lat: coord.latLng.lat(),
       lng: coord.latLng.lng(),
@@ -119,59 +113,63 @@ class MapContainer extends React.Component {
     })
   }
 
-  dragItem(newItems) {
+  dragItem = (newItems) => {
     this.setState({ items: newItems })
   }
 
   render() {
     const initialStyle = {
-    maxWidth: '700px',
-    height: '700px',
-    width: '100%',
-    overflow: 'hidden'
-  }
-  return (
-    <div className="wrapper">
-      <div className="wrapper__map">
-      <Map initialCenter={{
-        lat: this.state.position.lat,
-        lng: this.state.position.lng}}
-        onDragend={this.centerMoved} 
-        style={initialStyle} 
-        google={this.props.google} 
-        zoom={14}>
-        {
-         this.state.items.map((item, index)=> {
-            return  <Marker 
-            onClick={this.onMarkerClick} 
-            draggable={true}
-            name={item.value} 
-            key={index}
-            position={{lat: item.lat, lng: item.lng}} 
-            onDragend={(t, map, coord) => this.dragMarker(coord, index, item)} />
-          })
-        }
-        <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
-          <div>
-            <h3>{this.state.title}</h3>
+      maxWidth: '700px',
+      height: '500px',
+      width: '100%',
+      overflow: 'hidden'
+    }
+    return (
+      <div>
+        <div class="title">Введите пункт назначение, маркер появится в центре, также можно поменять пункты местами(dragndrop) и менять положение на карте</div>
+        <div className="wrapper">
+          <div className="wrapper__map">
+            <Map initialCenter={{
+              lat: this.state.position.lat,
+              lng: this.state.position.lng
+            }}
+              onDragend={this.centerMoved}
+              style={initialStyle}
+              google={this.props.google}
+              zoom={14}>
+              {
+                this.state.items.map((item, index) => {
+                  return <Marker
+                    onClick={this.onMarkerClick}
+                    draggable={true}
+                    name={item.value}
+                    key={index}
+                    position={{ lat: item.lat, lng: item.lng }}
+                    onDragend={(t, map, coord) => this.dragMarker(coord, index, item)} />
+                })
+              }
+              <InfoWindow marker={this.state.activeMarker} visible={this.state.showingInfoWindow}>
+                <div>
+                  <h3>{this.state.title}</h3>
+                </div>
+              </InfoWindow>
+              <Polyline
+                path={this.state.items.map((item) => { return item; })}
+                strokeColor="#0004ff"
+                strokeOpacity={1}
+                strokeWeight={4} />
+            </Map>
           </div>
-        </InfoWindow>  
-        <Polyline 
-          path={this.state.items.map((item)=> {return item;})} 
-          strokeColor="#0004ff" 
-          strokeOpacity={1} 
-          strokeWeight={4} />
-      </Map>
-      </div>
-      <div className="wrapper__list list">
-        <div className="list__input">
-          <input onKeyUp={this.addMarkerKeyEnter}  ref={this.inputRef} type="text"/>
-          <button onClick={this.addMarker}>Добавить</button>
-        </div>  
-      <List dragItemProps={this.dragItem} list={this.state.items}/>
-    </div>
-  </div>);
-  }  
+          <div className="wrapper__list list">
+            <div className="list__input">
+              <input onKeyUp={this.addMarkerKeyEnter} ref={this.inputRef} type="text" />
+              <button onClick={this.addMarker}>Добавить</button>
+            </div>
+            <List dragItemProps={this.dragItem} list={this.state.items} />
+          </div>
+        </div>
+      </div>);
+  }
 }
 export default GoogleApiWrapper({
   apiKey: (API_KEY)
